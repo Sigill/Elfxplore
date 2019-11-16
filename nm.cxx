1170fc2119be4e9a834e52bb942a9eae227e41cc
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2019-11-16 23:42:51 +0100

    Match nm lines manually instead of using a regex (better perf, less safe)

--- nm.cxx
+++ nm.cxx
@@ -1,14 +1,14 @@
 #include "nm.hxx"
 
-#include <regex>
 #include <boost/process.hpp>
+#include <string.h> // memrchr
 
 namespace bp = boost::process;
 
 SymbolSet nm(const std::string& file, const std::string& options) {
   SymbolSet symbols;
 
-  const std::regex symbol_regex("^.{16}(?: (.{16}))? (.) (.*$)");
+//  const std::regex symbol_regex("^.{16}(?: (.{16}))? (.) (.*$)");
 
   const std::string cmd = "nm " + options + " \"" + file + "\"";
 
@@ -17,23 +17,38 @@ SymbolSet nm(const std::string& file, const std::string& options) {
   bp::child c(cmd, bp::std_out > pipe_stream);
 
   std::string line;
-  std::smatch nm_match;
+//  std::smatch nm_match;
   while (pipe_stream && std::getline(pipe_stream, line) && !line.empty()) {
-    if (std::regex_match(line, nm_match, symbol_regex)) {
-      std::string name = nm_match[3];
+    long long sz = -1;
+    size_t offset = 17;
 
-      // Filter-out:
-      // .LC??
-      // _GLOBAL__sub_I_*.cpp
-      // symbol [clone .cold]
-      // DW.ref.__gxx_personality_v0
-      if (name[0] == '.' // There's a lot of .LC??
-          || name.rfind('.') != std::string::npos)
+    if (line[offset] >= '0' && line[offset] <= '9') {
+      const std::string size_str(line, 17, 16);
+      sz = std::stoll(size_str, nullptr, 16);
+      offset += 17;
+    }
+
+    // Filter-out:
+    // .LC??
+    // _GLOBAL__sub_I_*.cpp
+    // symbol [clone .cold]
+    // DW.ref.__gxx_personality_v0
+    if (line[offset + 2] == '.' // There's a lot of .LC??
+        || memrchr(&(line[offset + 2]), '.', line.length() - offset - 2) != NULL)
       continue;
 
-      long long sz = (nm_match[1].length() == 0) ? -1 : std::stoll(nm_match[1], nullptr, 16);
-      symbols.emplace(std::move(name), nm_match[2], sz);
-    }
+    symbols.emplace(std::string(line, offset + 2), std::string(line, offset, 1), sz);
+
+//    if (std::regex_match(line, nm_match, symbol_regex)) {
+//      std::string name = nm_match[3];
+
+//      if (name[0] == '.' // There's a lot of .LC??
+//          || name.rfind('.') != std::string::npos)
+//      continue;
+
+//      long long sz = (nm_match[1].length() == 0) ? -1 : std::stoll(nm_match[1], nullptr, 16);
+//      symbols.emplace(std::move(name), nm_match[2], sz);
+//    }
   }
 
   c.wait();
